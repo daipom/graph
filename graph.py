7161e4c28e11f70789f308a410a5aa8da80238de
daipom <reangoapyththeorem@gmail.com>
2019-01-09 11:36:23 +0900

    add multi y_axis function

--- graph.py
+++ graph.py
@@ -7,7 +7,15 @@ import plotly.graph_objs as go
 from typing import List
 
 
-def create_graph_datalist(df: DataFrame, path: str="", is_x_time: bool=True, time_format: str="%m/%d/%Y %H:%M:%S", use_elapsed: bool=False, specified_column: str="") -> List[go.Scatter]:
+def create_graph_datalist(
+    df: DataFrame,
+    path: str="",
+    is_x_time: bool=True,
+    time_format: str="%m/%d/%Y %H:%M:%S",
+    use_elapsed: bool=False,
+    yaxis_columns: List[str]=None,
+    yaxis2_columns: List[str]=None
+) -> List[go.Scatter]:
     x_values = None
     if is_x_time:
         if use_elapsed:
@@ -18,7 +26,7 @@ def create_graph_datalist(df: DataFrame, path: str="", is_x_time: bool=True, tim
     else:
         x_values = df.iloc[:, 0]
 
-    if specified_column is None or specified_column == "None" or specified_column == "":
+    if (yaxis_columns is None or not any(yaxis_columns)) and (yaxis2_columns is None or not any(yaxis2_columns)):
         return [
             go.Scatter(
                 x=x_values,
@@ -31,24 +39,59 @@ def create_graph_datalist(df: DataFrame, path: str="", is_x_time: bool=True, tim
         return [
             go.Scatter(
                 x=x_values,
-                y=df[specified_column],
-                name=path + ": " + specified_column,
+                y=df[column_name],
+                name=path + ": " + column_name,
                 opacity=0.50,
-            )
+            ) for column_name in yaxis_columns
+        ] + [
+            go.Scatter(
+                x=x_values,
+                y=df[column_name],
+                name=path + ": " + column_name,
+                opacity=0.50,
+                yaxis="y2"
+            ) for column_name in yaxis2_columns
         ]
 
 
-def plot(dfs: List[DataFrame], pathlist: List[str], is_x_time: bool=True, time_format: str="%m/%d/%Y %H:%M:%S", to_jupyter: bool=False, specified_column: str="", use_elapsed: bool=False):
+def plot(
+    dfs: List[DataFrame],
+    pathlist: List[str],
+    is_x_time: bool=True,
+    time_format: str="%m/%d/%Y %H:%M:%S",
+    to_jupyter: bool=False,
+    use_elapsed: bool=False,
+    yaxis_title: str="Bytes",
+    yaxis2_title: str="",
+    yaxis_columns: List[str]=None,
+    yaxis2_columns: List[str]=None
+):
     data = []
     for df, path in zip(dfs, pathlist):
-        data += create_graph_datalist(df, path=path, is_x_time=is_x_time, time_format=time_format, use_elapsed=use_elapsed, specified_column=specified_column)
+        data += create_graph_datalist(
+            df,
+            path=path,
+            is_x_time=is_x_time,
+            time_format=time_format,
+            use_elapsed=use_elapsed,
+            yaxis_columns=yaxis_columns,
+            yaxis2_columns=yaxis2_columns
+        )
 
     fig = go.Figure(
         data=data,
         layout=go.Layout(
             title="",
-            xaxis=dict(title="Elapsed [sec]"),
-            yaxis=dict(title="Bytes", exponentformat="SI")
+            xaxis=dict(title="Elapsed [sec]" if use_elapsed else "X"),
+            yaxis=dict(title=yaxis_title, exponentformat="SI"),
+            yaxis2=dict(
+                title=yaxis2_title if yaxis2_columns is not None and yaxis2_columns != "None" else "",
+                exponentformat="SI",
+                overlaying="y",
+                side="right",
+                titlefont=dict(color="rgb(148, 103, 189)"),
+                tickfont=dict(color="rgb(148, 103, 189)")
+            )
         )
     )
 
@@ -59,10 +102,30 @@ def plot(dfs: List[DataFrame], pathlist: List[str], is_x_time: bool=True, time_f
         py.iplot(fig)
 
 
-def main(file_path_list: List[str], encoding: str, is_x_time: bool, time_format: str, specified_column: str, use_elapsed: bool):
+def main(
+    file_path_list: List[str],
+    encoding: str,
+    is_x_time: bool,
+    time_format: str,
+    use_elapsed: bool,
+    yaxis_title: str,
+    yaxis2_title: str,
+    yaxis_columns: List[str],
+    yaxis2_columns: List[str]
+):
     dfs = map(lambda path: pd.read_csv(path, encoding=encoding), file_path_list)
     # df = pd.read_csv(file_path, encoding=encoding)
-    plot(dfs, file_path_list, is_x_time=is_x_time, time_format=time_format, specified_column=specified_column, use_elapsed=use_elapsed)
+    plot(
+        dfs,
+        file_path_list,
+        is_x_time=is_x_time,
+        time_format=time_format,
+        use_elapsed=use_elapsed,
+        yaxis_title=yaxis_title,
+        yaxis2_title=yaxis2_title,
+        yaxis_columns=yaxis_columns,
+        yaxis2_columns=yaxis2_columns
+    )
 
 
 if __name__ == '__main__':
@@ -71,9 +134,22 @@ if __name__ == '__main__':
     parser.add_argument("--encoding", help="Encoding option of dataframe. Default: utf-16", type=str, default="utf-16")
     parser.add_argument("--is_x_time", help="To handle the first column as time value. Default: True", type=lambda s: s.lower() in ["true", "1", "yes"], default="True")
     parser.add_argument("--time_format", help="Time format for the first column. Default: %m/%d/%Y %H:%M:%S", type=str, default="%m/%d/%Y %H:%M:%S")
-    parser.add_argument("--specify_column", help="If column name is specified here, then only plot the column as Y every one file. If not specified, then plot all columns as Y except the first column (X).", type=str, default="None")
     parser.add_argument("--elapsed", help="To convert the first column to elapsed time. Default: False", type=lambda s: s.lower() in ["true", "1", "yes"], default="False")
+    parser.add_argument("--yaxis_title", help="Title of Y axis. Default: Bytes", type=str, default="Bytes")
+    parser.add_argument("--yaxis2_title", help="Title of Y axis2.", type=str, default="None")
+    parser.add_argument("--yaxis_columns", help="If column names are specified here, then plot those columns to yaxis1. If this and 'yaxis2_columns' are not specified, then plot all columns except the first as X to yaxis1.", nargs="*")
+    parser.add_argument("--yaxis2_columns", help="If column names are specified here, then plot those columns to yaxis2.", nargs="*")
 
     args = parser.parse_args()
 
-    main(args.path_list, args.encoding, args.is_x_time, args.time_format, args.specify_column, args.elapsed)
+    main(
+        args.path_list,
+        args.encoding,
+        args.is_x_time,
+        args.time_format,
+        args.elapsed,
+        args.yaxis_title,
+        args.yaxis2_title,
+        args.yaxis_columns,
+        args.yaxis2_columns
+    )
